fdd250098270383d381d89b048e7ef7d276e1828
Sowjanya2005 <159280328+Sowjanya2005@users.noreply.github.com>
2024-05-07 13:06:05 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,7 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
-output[7:0]y;
-
-
-
+module encoder8x3(i,o);
+input [7:0]i;
+output [2:0]o;
+or g1(o[0],i[1],i[3],i[5],i[7]);
+or g2(o[1],i[2],i[3],i[6],i[7]);
+or g3(o[2],i[7],i[6],i[5],i[4]);
 endmodule
