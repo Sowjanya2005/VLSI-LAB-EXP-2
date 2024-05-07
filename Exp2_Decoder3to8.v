cb7f8a781b0e7191f6c967cdcbb6356466166957
Sowjanya2005 <159280328+Sowjanya2005@users.noreply.github.com>
2024-05-07 13:08:03 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,16 @@
-module encoder8x3(i,o);
-input [7:0]i;
-output [2:0]o;
-or g1(o[0],i[1],i[3],i[5],i[7]);
-or g2(o[1],i[2],i[3],i[6],i[7]);
-or g3(o[2],i[7],i[6],i[5],i[4]);
+module decoder3x8(a,y);
+input [2:0]a;
+output [7:0]y;
+wire w1,w2,w3;
+not g1(w1,a[0]);
+not g2(w2,a[1]);
+not g3(w3,a[2]);
+and g4(y[0],w1,w2,w3);
+and g5(y[1],a[0],w2,w3);
+and g6(y[2],w1,a[1],w3);
+and g7(y[3],a[0],a[1],w3);
+and g8(y[4],w1,w2,a[2]); 
+and g9(y[5],a[0],w2,a[2]);
+and g10(y[6],w1,a[1],a[2]);
+and g11(y[7],a[0],a[1],a[2]);
 endmodule
